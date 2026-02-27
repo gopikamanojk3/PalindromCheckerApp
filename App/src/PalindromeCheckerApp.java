65e5201c74822972923afc385c10d664c149db12
gopikamanojk3 <gopika.manojk3@gmail.com>
2026-02-27 09:41:56 +0530

    UC5

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,25 +1,24 @@
 import java.util.*;
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        String input = "radar";
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
+    public static void main(String[] args) {
+
+
+        String input = "noon";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
         boolean isPalindrome = true;
-        while (start < end)
-        {
-            if (chars[start] != chars[end])
-            {
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
         System.out.println("Input: " + input);
         System.out.println("Is Palindrome? " + isPalindrome);
     }
-
 }
 
 
